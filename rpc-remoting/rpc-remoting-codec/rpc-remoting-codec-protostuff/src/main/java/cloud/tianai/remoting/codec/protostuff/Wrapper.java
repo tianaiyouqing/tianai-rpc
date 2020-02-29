99ee2913bcffab0d97894437c8a840d19afeea50
tianaiyouqing@163.com <tianaiyouqing@163.com>
2020-02-29 20:27:11 +0800

    增加protostuff序列化技术

---
+++ rpc-remoting/rpc-remoting-codec/rpc-remoting-codec-protostuff/src/main/java/cloud/tianai/remoting/codec/protostuff/Wrapper.java
@@ -0,0 +1,16 @@
+package cloud.tianai.remoting.codec.protostuff;
+
+/**
+ * Protostuff can only serialize/deserialize POJOs, for those it can't deal with, use this Wrapper.
+ */
+public class Wrapper<T> {
+    private T data;
+
+    Wrapper(T data) {
+        this.data = data;
+    }
+
+    Object getData() {
+        return data;
+    }
+}
